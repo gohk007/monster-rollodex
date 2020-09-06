3a9a1f5264f1f32d47e4901fa34fd51c65ee3884
unknown <gohk007@gmail.com>
2020-09-06 11:24:59 +0530

    Updates

---
+++ precache-manifest.5141c0bd93b8722b0bc483eb26ea3d8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437821c3976015ce9375c9e1a33fe28e",
+    "url": "/gohk007.github.io/monster-rollodex/index.html"
+  },
+  {
+    "revision": "ad56fe70141d4dc08673",
+    "url": "/gohk007.github.io/monster-rollodex/static/css/main.c16ad2f6.chunk.css"
+  },
+  {
+    "revision": "2b3b7f6fdce3c4257c1a",
+    "url": "/gohk007.github.io/monster-rollodex/static/js/2.e499a0ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gohk007.github.io/monster-rollodex/static/js/2.e499a0ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad56fe70141d4dc08673",
+    "url": "/gohk007.github.io/monster-rollodex/static/js/main.1dc73ed1.chunk.js"
+  },
+  {
+    "revision": "587fa13852093bd98563",
+    "url": "/gohk007.github.io/monster-rollodex/static/js/runtime-main.b8fd678a.js"
+  }
+]);
